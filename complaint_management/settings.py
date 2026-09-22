c14dfe3d527b5066f32058c7ef255eda1fa44418
Akhilan-hub <akhilanakhilan45@gmail.com>
2026-09-22 14:35:34 +0530

    Configure Render deployment settings and build script

--- complaint_management/settings.py
+++ complaint_management/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -113,6 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# CSRF Trusted Origins for Render
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
 
 # Media files (Uploaded user & resolution images)
 MEDIA_URL = '/media/'
